4315c73664efe1ccf0a33cf459339c33ae891476
jeanwanjiru <jeanwanjiru06@gmail.com>
2023-06-13 11:45:39 -0400

    creats arect router routes

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,8 +1,34 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 
+const linkStyles = {
+  display: "flex",
+  width: "50px",
+  padding: "12px",
+  margin: "0 6px 6px",
+  background: "",
+  textDecoration: "none",
+  color: "blue",
+};
+
 function NavBar() {
-  return <div>{/*{code here}*/}</div>;
+  return (
+    <div className="navbar">
+      <NavLink to="/" style={linkStyles}>
+        Home
+      </NavLink>
+
+      <NavLink to="/movies" style={linkStyles}>
+        Movies
+      </NavLink>
+      <NavLink to="/directors" style={linkStyles}>
+        Directors
+      </NavLink>
+      <NavLink to="/actors" style={linkStyles}>
+        Actors
+      </NavLink>
+    </div>
+  );
 }
 
 export default NavBar;
